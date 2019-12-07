3ca61d79a77fe1e92967c24fa3f11758eadf40e3
q942014528 <942014528@qq.com>
2019-12-07 14:47:01 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=q942014528
 
 export MYCLUSTER=tektonknativebeijing19
-export KUBECONFIG=tektonknativebeijing19-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing19/kube-config-tok04-tektonknativebeijing19.yml
 
 export NAMESPACE=1840867
 export EMAIL=wyhuang_hao@163.com
-export INGRESS=https://github.com/daisy-ycguo/devopslab/blob/master/00-install/02-k8s-connect.md
+export INGRESS=tektonknativebeijing19-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
